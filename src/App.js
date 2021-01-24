274fb1e4734d59772612ebc0c9c7dad26ed196b8
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2021-01-24 15:59:23 +0100

    Add Routes in app.js for listpage and detailpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Route, Switch } from "react-router-dom"
+import JobDetailPage from "./pages/JobDetailPage"
+import JobListPage from "./pages/JobListPage"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Switch>
+				<Route path="/jobs/:id" component={JobDetailPage} />
+				<Route path="/jobs" component={JobListPage} />
+			</Switch>
+		</div>
+	)
 }
 
-export default App;
+export default App
