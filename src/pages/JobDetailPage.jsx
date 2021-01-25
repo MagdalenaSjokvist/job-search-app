de981aee0588fc8775f613d2fbbb34d7173e0b2c
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2021-01-25 15:53:58 +0100

    Render correct job details on detail page

--- src/pages/JobDetailPage.jsx
+++ src/pages/JobDetailPage.jsx
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import JobListItem from "../components/JobListItem"
 
-export default function JobDetailPage() {
+export default function JobDetailPage(props) {
+	const id = props.match.params.id
+	const [job, setJob] = useState(null)
+
+	function getJobDetails() {
+		const url = `https://us-central1-wands-2017.cloudfunctions.net/githubjobs?id=${id}`
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => setJob(data))
+	}
+
+	useEffect(() => {
+		getJobDetails()
+	}, [])
+
 	return (
 		<div>
 			<h1>Job Detail Page</h1>
+			{job && <JobListItem job={job} />}
 		</div>
 	)
 }
